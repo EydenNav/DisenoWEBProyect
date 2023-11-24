a7506d168179f4ec92a725252dc2eb161cfe44fa
EydenNav <al060113@uacam.mx>
2023-11-24 14:14:01 -0600

    ProyectoDW
    Proyecto

---
+++ Proyecto Visual studio/Script.js
@@ -0,0 +1,22 @@
+function hacerCircular(elementoId) {
+    var imagen = document.getElementById(elementoId);
+    imagen.style.borderRadius = '50%';
+}
+
+hacerCircular('gato');
+hacerCircular('ayp');
+
+let videoList = document.querySelectorAll('.video-list-container .list');
+
+videoList.forEach(vid =>{
+   vid.onclick = () =>{
+      videoList.forEach(remove =>{remove.classList.remove('active')});
+      vid.classList.add('active');
+      let src = vid.querySelector('.list-video').src;
+      let title = vid.querySelector('.list-title').innerHTML;
+      document.querySelector('.main-video-container .main-video').src = src;
+      document.querySelector('.main-video-container .main-video').play();
+      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+      document.querySelector('.main-video-container .main-video').classList.add('video-activado');
+   };
+});
